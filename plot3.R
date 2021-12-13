f02df3759f95351cafbbde6d4ce35254d7a6759b
MySunnySoon <92298539+MySunnySoon@users.noreply.github.com>
2021-12-13 11:23:31 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,8 +1,8 @@
 # source("plot3.R")
+png("plot3.png", width=480, height=480)
 plot(x = hpcs$DateTime, y = hpcs$Sub_metering_1, xlab="", type="l", ylab="Energy sub metering")
 lines(x = hpcs$DateTime, y = hpcs$Sub_metering_2, col="red")
 lines(x = hpcs$DateTime, y = hpcs$Sub_metering_3, col="blue")
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1,legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png, file="plot3.png", width=480, height=480)
 dev.off()
 
